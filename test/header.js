82c084d7881882d91f04e9cfc01626978d295b0c
kurkim0661 <48935841+kurkim0661@users.noreply.github.com>
2020-02-12 14:41:12 +0900

    Create header.js

---
+++ test/header.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+
+    document.getElementById("header").innerHTML='<object type="text/html" data="https://kurkim0661.github.io/WEB2/test/header.html"></object>';
+
+})
+
