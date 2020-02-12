8c03728d998891c9050252ef483a971545ac3db4
kurkim0661 <48935841+kurkim0661@users.noreply.github.com>
2020-02-12 14:39:09 +0900

    Create footer.js

---
+++ test/footer.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    document.getElementById("footer").innerHTML='<object type="text/html" data="https://kurkim0661.github.io/WEB2/test/footer.html"></object>';
+})
+
+
